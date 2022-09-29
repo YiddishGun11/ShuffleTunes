d3776793e8951d1446a4964f5e156efd093c972a
Maisin Edouard <92255720+YiddishGun11@users.noreply.github.com>
2022-09-29 16:24:06 +0200

    Rework Connexion/Inscription 🔨

--- WebApp/client/src/pages/Connexion/ConnexionPage.js
+++ WebApp/client/src/pages/Connexion/ConnexionPage.js
@@ -7,31 +7,21 @@ function ConnexionPage(){
 
     const[display, setDisplay] = useState(true);
 
-    if(display){
-
-        return(
-            <div>
-                <div className='home-menu'>
-                    <Link to="/" className='home-menu-logo'><p>ShuffleTunes</p></Link>
-                    <Link to="/connexion" className='home-menu-link'><p>Login</p></Link>
-                </div>
-                <Connexion display={display} setDisplay={setDisplay} />
+    return(
+        <div>
+            <div className='home-menu'>
+                <Link to="/" className='home-menu-logo'><p>ShuffleTunes</p></Link>
+                <Link to="/connexion" className='home-menu-link'><p>Login</p></Link>
             </div>
-
-        );
-    }
-
-    else{
-        return(
-            <div>
-                <div className='home-menu'>
-                    <Link to="/" className='home-menu-logo'><p>ShuffleTunes</p></Link>
-                    <Link to="/connexion" className='home-menu-link'><p>Login</p></Link>
-                </div>
+            {display?(
+                <Connexion display={display} setDisplay={setDisplay}/>
+            ):
+            (
                 <Inscription display={display} setDisplay={setDisplay} />
-            </div>
-        );
-    }
+            )}
+        </div>
+
+    );
 }
 
 export default ConnexionPage;
