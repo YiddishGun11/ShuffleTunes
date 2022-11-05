7b5aaa69690dafbcd8dc5f2938417befe9f72d50
YiddishGun11 <edouard.mais1@icloud.com>
2022-11-05 11:44:05 +0100

    [UPDATE] upgrading App.js performances 🚀
    - Using Lazy and Suspense to upgrade performances of the page on loading
    (avoiding launching each imported components at same time)

--- WebApp/client/src/pages/App.js
+++ WebApp/client/src/pages/App.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, {lazy, Suspense} from 'react'
 import {Routes, Route} from 'react-router-dom'
 import "./App.css"
-import Home from "../pages/Home/Home"
-import ConnexionPage from './Connexion/ConnexionPage'
-import DashBoard from './DashBoard/DashBoard'
+
+const Home = lazy(()=> import ('./Home/Home'));
+const Login = lazy(()=> import('./Connexion/ConnexionPage'));
+const DashBoard = lazy(()=> import ('./DashBoard/DashBoard'));
 
 function App() {
     return (
         <div className="App">
-            <Routes>
-                <Route path="/" element={ <Home /> } />
-                <Route path="/connexion" element={ <ConnexionPage/> } />
-                <Route path="/dashboard" element={<DashBoard />} />
-            </Routes>
+            <Suspense fallback={<h1 className='loading-message'>Loading...</h1>}>
+                <Routes>
+                    <Route path="/" element={ <Home /> } />
+                    <Route path="/connexion" element={ <Login/> } />
+                    <Route path="/dashboard" element={<DashBoard />} />
+                </Routes>
+            </Suspense >
         </div>
     )
 }
