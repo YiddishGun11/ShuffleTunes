b67c5de4f2e4efcadd63134ae4231426b23f17a8
YiddishGun11 <edouard.mais1@icloud.com>
2022-11-05 11:44:05 +0100

    [UPDATE] upgrading App.js performances 🚀
    - Using Lazy and Suspense to upgrade performances of the page on loading
    (avoiding launching each imported components at same time)

--- WebApp/client/src/pages/Connexion/ConnexionPage.js
+++ WebApp/client/src/pages/Connexion/ConnexionPage.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import './ConnexionPage.scss'
 import {Link} from 'react-router-dom'
 import Connexion from '../../components/Connexion/Connexion'
 import Inscription from '../../components/Inscription/Inscription';
