a31982ab990f8425042128a7c7e71d1d33379b25
M-Momin <m.momin@students.ephec.be>
2022-11-24 12:35:15 +0100

    [UPDATE : Ajout d'un route]
    Ajout d'une route pour le lancement d'un fichier .wav sur le raspberry.

--- WebApp/server/routes/routes.js
+++ WebApp/server/routes/routes.js
@@ -17,6 +17,7 @@ const createPlaylistValidation = require('../validation/createPlaylistValidation
 // secutity ùidelware
 const rateLimit = require('../controllers/rateLimiter.js');
 
+const exec = require('../controllers/exec')
 
 // ROUTER USE
 router.use(rateLimit.limiter)
@@ -40,6 +41,9 @@ router.get('/songs/:id', data.getSongs);
 
 
 //POST REQUESTS
+
+//play song
+router.post('/pd', exec.playSong);
 
 // send the music we want to upload to the server
 router.post('/createplaylist', createPlaylistValidation.createPlaylistValidation, data.createPlaylist);
