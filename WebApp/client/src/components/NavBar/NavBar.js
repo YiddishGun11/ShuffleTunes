a102233bf8d14a212ff3faa04c973bfaf7f152a4
M-Momin <m.momin@students.ephec.be>
2022-12-08 11:45:19 +0100

    [UPDATE : Récupération du nom de l'utilisateur + opti]
    Récupération du nom du profil, suppression du bouton de modification et
    option de changement d'image disponible mais component dédié non
    implémenté.

--- WebApp/client/src/components/NavBar/NavBar.js
+++ WebApp/client/src/components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react';
+import React, {useContext, useReducer} from 'react';
 import './NavBar.scss';
 
 import { ThemeContext } from '../Context/ThemeContext';
@@ -9,11 +9,27 @@ import {URL} from '../../scripts/url'
 import {BsPersonLinesFill, BsGear} from 'react-icons/bs'
 import {BiLogOut} from 'react-icons/bi'
 
+//import Profil and Settings components
 import Profil from '../Profil/Profil';
-
 import Settings from '../Settings/Settings';
 
+const displayState = {display: 0};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'settings':
+      return {display: 1};
+    case 'profile':
+      return {display: 2};
+    default:
+      return {display: 0}
+  }
+}
+
 function NavBar({setContentDisplay}){
+
+    //useReducer to manage display between Profile and UserSettings component
+    const [state, dispatch] = useReducer(reducer, displayState);
 
     const {theme} = useContext(ThemeContext);
 
@@ -39,16 +55,16 @@ function NavBar({setContentDisplay}){
             </div>
             <div className='navbar-section-icons'>
                 <div className='navbar-section-icons-childs'>
-                    <BsPersonLinesFill size={20}  onClick={()=>handleDisplay()} id="navbar-icon"/>
-                    {display ?(
+                    <BsPersonLinesFill size={20}  onClick={()=>dispatch({type: "profile"})} id="navbar-icon"/>
+                    {state.display === 2 ?(
                         <Profil />
                     ):(
                         <div></div>
                     )}
                 </div>
                 <div className='navbar-section-icons-childs'>      
-                    <BsGear size={20} onClick={()=>handleDisplay()} id="navbar-icon"/>
-                    {display ?(
+                    <BsGear size={20} onClick={()=>dispatch({type: "settings"})} id="navbar-icon"/>
+                    {state.display === 1 ?(
                         <Settings/>
                     ):(
                         <div></div>
--- WebApp/client/src/components/Profil/Profil.js
+++ WebApp/client/src/components/Profil/Profil.js
@@ -1,30 +1,53 @@
-import React, {useState} from 'react'
 import "./Profil.scss";
 
-import logo from './Utepils.png';
-import {FaRegEdit} from 'react-icons/fa'
+import { useEffect, useState } from "react";
+
+import axios from 'axios'
+
+import {BiError} from 'react-icons/bi'
+
+import logo from '../../img/profil-image.png';
+
+//importing dynamic url
+import {URL} from '../../scripts/url'
 
 
 function Profil(){
+
+    const [error, setError] = useState([]);
+    const [data, setData] = useState([]);
+
+    useEffect(() =>{
+        axios.get(URL + '/user/' + 4 )
+            .then((response) =>{
+                setData(response.data[0][0])
+            })
+            .catch((error) =>{
+                setError(error);
+            })
+    }, []);
+
+
     return(
         <section className='profil-container'>
-            <div>
-                <div className='profil-items'>
-                    <div>
-                        <FaRegEdit size={22} className="edit-icons"/>
-                    </div>
-                </div>
-                <div className='profil-image'>
-                    <div>
+            {error.length === 0 ?(
+                <>
+                    <div className='profil-image'>
                         <img src={logo} alt="Logo" width="150" height="150"/>
                     </div>
-                </div>
-                <div className='profil-name'>
-                    <div>
-                        <p> Utepils</p>
+                    <div className="change-image-div">
+                        <p>Change Photo ?</p>
                     </div>
+                    <div className='profil-name'>
+                        <p>{data.pseudo}</p>
+                    </div>
+                </>
+            ):(
+                <div className="profil-error-message">
+                    <p>An error just occured...</p>
+                    <span><BiError size={18} className="error-icon"/></span> 
                 </div>
-            </div>
+            )}
         </section>
     )
 }
