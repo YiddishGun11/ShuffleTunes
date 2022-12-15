5c8da1b91c4cee895acd8a1ef019e095972fbea5
M-Momin <m.momin@students.ephec.be>
2022-12-15 11:00:14 +0100

    UPDATE : Modification des erreurs liées aux extensions

--- WebApp/server/controllers/exec.js
+++ WebApp/server/controllers/exec.js
@@ -8,7 +8,19 @@ const playSong = async(request, response) => {
     let data = request.body;
     let song = data.song;
     const username = await getUser.whoIsConnected(request.signedCookies);
-    
+
+    exec(`/home/pi/Music/${username}/ mpg123 -w ${song}.wav ${song}.mp3`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+
     exec(`/home/pi/play /home/pi/Music/${username}/${song}.wav`, (error, stdout, stderr) => {
     //exec(`start C:/Users/maxim/Desktop/Metallica-MasterOfPuppets(Lyrics).mp3`, (error, stdout, stderr) => {
         if (error) {
