b6385215a71b19ac8edd4355cd08bc58542cd0fa
Maisin Edouard <92255720+YiddishGun11@users.noreply.github.com>
2022-12-19 15:03:32 +0100

    [UPDATE] *SideBar css
    - adapting css rules to a better and proper sideBar after adapting code to tabulation

--- WebApp/client/src/components/SideBar/SideBar.js
+++ WebApp/client/src/components/SideBar/SideBar.js
@@ -15,26 +15,26 @@ function SideBar({setContentDisplay}){
     return(
         <div className={theme ? 'sidebar-dark' : 'sidebar-light'}>
             <div className='sidebar-item1'>
+                <span><BsFolder size={25} id="sidebar-icon"/></span>
                 <button onClick={()=>setContentDisplay(1)}>
-                    <BsFolder size={25} id="sidebar-icon"/>
                     <p>Your Files</p>
                 </button>
             </div>
-            <div className='sidebar-item2'>
+            <div className='sidebar-item1'>
+                <span><BsFolderPlus size={25} id="sidebar-icon"/></span>
                 <button onClick={() => setContentDisplay(3)}>
-                    <BsFolderPlus size={25} id="sidebar-icon"/>
                     <p>Upload new files</p>
                 </button>
             </div>
-            <div className='sidebar-item2'>
+            <div className='sidebar-item1'>
+            <span><MdOutlinePlaylistAdd size={25} id="sidebar-icon"/></span>
                 <button onClick={() => setContentDisplay(4)}>
-                    <MdOutlinePlaylistAdd size={25} id="sidebar-icon"/>
                     <p>Your PlayLists</p>
                 </button>
             </div>
-            <div className='sidebar-item2'>
+            <div className='sidebar-item1'>
+                <span><AiOutlineHeart size={25} id="sidebar-icon"/></span>
                 <button onClick={()=>setContentDisplay(2)} >
-                    <AiOutlineHeart size={25} id="sidebar-icon"/>
                     <p>Favorites Songs</p>
                 </button>
             </div>
