14a736922152f93018d3e31e898325231c0afea2
Maisin Edouard <92255720+YiddishGun11@users.noreply.github.com>
2022-12-19 14:42:19 +0100

    [NEW] Cookie component
    - Creating a new cookie component displaying on home page to ensure that user accepts their usage when using ShuffleTunes

--- WebApp/client/src/pages/Home/Home.js
+++ WebApp/client/src/pages/Home/Home.js
@@ -1,22 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import Image from '../../assets/landing-page-girl.png'
 import "./Home.scss"
 
+import { useState } from "react";
+
+import { Link } from 'react-router-dom';
+import Image from '../../assets/landing-page-girl.png'
+
+import Popup from "../../components/Cookies/Popup";
+
 function Home(){
+
+    const [display, setDisplay] = useState(true);
+
     return(
-        <div>
+        <div className="home-menu-main">
             <div className='home-menu'>
                 <Link to="/" className='home-menu-logo'><p>ShuffleTunes</p></Link>
                 <Link to="/connexion" className='home-menu-link'><p>Login</p></Link>
             </div>
 
             <div className='home-section'>
+                <>
+                    {display ? (
+                        <Popup setDisplay={setDisplay}/>
+                    ):(
+                        <></>
+                    )}
+                </>
                 <img src={Image} alt="" ></img>
                 <div className='home-section-div'>
-                    <h1>Share Your Music</h1>
-                    <p>Listen to your music on several speakers</p>
-                    <Link to="/connexion"><button>Join Our App</button></Link>
+                        <h1>Share Your Music</h1>
+                        <p>Listen to your music on several speakers</p>
+                        <Link to="/connexion"><button>Join Our App</button></Link>
                 </div>
             </div>
 
